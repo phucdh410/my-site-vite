a44dfec1bf1ceede66282a9a827f48efc4ee77fc
phucdh410 <phucdh410@gmail.com>
2024-04-01 16:34:54 +0700

    fix bug reload

--- src/utils/hooks/index.ts
+++ src/utils/hooks/index.ts
@@ -2,7 +2,11 @@ import { useEffect } from 'react';
 import { useSelector as useReduxSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
+import Cookies from 'js-cookie';
+
 import { IRootState } from '@/redux';
+
+import { setAuthToken } from '../axios/functions';
 
 export const useSelector = useReduxSelector.withTypes<IRootState>();
 
@@ -20,4 +24,11 @@ export const useAuthCheck = (layout: 'login' | 'main') => {
       navigate('/');
     }
   }, [isLogined, layout]);
+
+  useEffect(() => {
+    if (isLogined) {
+      const access_token = Cookies.get('token');
+      access_token && setAuthToken(access_token);
+    }
+  }, [isLogined]);
 };
